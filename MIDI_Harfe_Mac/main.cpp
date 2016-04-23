581d7f4beda602b2ccddd4b1de6e0f00d479ace2
nicobosshard <nico@bosshome.ch>
2016-04-23 16:11:49 +0200

    Säuberung des Quellcodes sowie Anpassungen der Fingerprint-Funktion

--- MIDI_Harfe_Mac/main.cpp
+++ MIDI_Harfe_Mac/main.cpp
@@ -110,74 +110,16 @@ vector<bool> Note_Play { 0,0,0,0,0,0,0, 0,0,0,0,0,0,0, 0,0,0,0,0,0,0, 0,0,0,0,0,
 char code_ascii;
 
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <ifaddrs.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#ifdef __linux__
-#    include <sys/ioctl.h>
-#    include <netinet/in.h>
-#    include <unistd.h>
-#    include <string.h>
 
-//    Match Linux to FreeBSD
-#    define AF_LINK AF_PACKET
-#else
-#    include <net/if_dl.h>
-#endif
 
 #include <stdio.h>
 #include <stdlib.h>
-
-const sockaddr_in* castToIP4(const sockaddr* addr) {
-   if (addr == NULL) {
-      return NULL;
-   } else if (addr->sa_family == AF_INET) {
-      // An IPv4 address
-      return reinterpret_cast<const sockaddr_in*>(addr);
-   } else {
-      // Not an IPv4 address
-      return NULL;
-   }
-}
-
-
-/** Assumes addr is in host byte order */
-void printIP(const char* name, int addr) {
-   printf("%s = %3d.%3d.%3d.%3d\n", name, (addr >> 24) & 0xFF, (addr >> 16) & 0xFF,
-          (addr >> 8) & 0xFF, addr & 0xFF);
-}
-
-
-void printIP(const char* name, const sockaddr_in* addr) {
-   if (addr != NULL) {
-      printIP(name, ntohl(addr->sin_addr.s_addr));
-   }
-}
-
-const char* levelToString(int level) {
-   switch(level) {
-      case AF_INET:
-         return "Internet (AF_INET)";
-         
-      case AF_LINK:
-         return "Link (AF_LINK/AF_PACKET)";
-      default:
-         return "Other";
-   }
-}
-
-
-
-
 
 
 
 #include <libgen.h>
 
 
-string GetIP(void);
 string get_sys_info(void);
 string get_cpu_freq_max(void);
 string statvfs_vfs(void);
@@ -876,7 +818,7 @@ void GetHash(void)
    
    string sys_fingerprint;
    stringstream sys_fingerprint_ss;
-   sys_fingerprint_ss << GetIP() << get_cpu_freq_max() << get_sys_info() << statvfs_vfs();
+   sys_fingerprint_ss << get_cpu_freq_max() << get_sys_info() << statvfs_vfs();
    sys_fingerprint=sys_fingerprint_ss.str();
    cout << sys_fingerprint << endl;
 
@@ -912,121 +854,42 @@ void Generate(void)
 
 
 
+/*
+#include <assert.h>
+#include <SystemConfiguration/SystemConfiguration.h>
 
-
-
-
-
-
-
-/**
- http://cs.williams.edu/~morgan/code/C++/getip.cpp
- 
- @file getip.cpp
- 
- @author Morgan McGuire, morgan@cs.williams.edu
- @date   November 2008
- 
- Enumerates the "interfaces" on a Unix (e.g. OS X, Linux, FreeBSD)
- system.  This includes what we consider to be the Ethernet and
- Wireless adapters.
- 
- This code will:
- 
- - get the IP address
- - get the broadcast address
- - get the subnet mask
- 
- and print them for all such devices.  This is similar functionality
- to the Unix program ifconfig.  The purpose of this program is to
- demonstrate how to access that functionality within a C program.
- 
- */
-
-string GetIP(void) {
+int getUsername( void ) {
    
-   stringstream MAC_address_ss;
-   string MAC_address;
+
+   CFStringRef result = SCDynamicStoreCopyConsoleUser(NULL, NULL, NULL);
+
+ 
+   SCDynamicStoreRef store;
+   CFStringRef name;
+   uid_t uid;
+#define BUFLEN 256
+   char buf[BUFLEN];
+   Boolean ok;
    
-   // Head of the interface address linked list
-   ifaddrs* ifap = NULL;
+   store = SCDynamicStoreCreate(NULL, CFSTR("GetConsoleUser"), NULL, NULL);
+   assert(store != NULL);
+   name = SCDynamicStoreCopyConsoleUser(store, &uid, NULL);
+   CFRelease(store);
    
-   int r = getifaddrs(&ifap);
-   
-   if (r != 0) {
-      // Non-zero return code means an error
-      printf("return code = %d\n", r);
-      exit(r);
+   if (name != NULL) {
+      ok = CFStringGetCString(name, buf, BUFLEN, kCFStringEncodingUTF8);
+      assert(ok == true);
+      CFRelease(name);
+   } else {
+      strcpy(buf, "<none>");
    }
+
+   cout << result << endl;
    
-   ifaddrs* current = ifap;
-   
-   if (current == NULL) {
-      printf("No interfaces found\n");
-   }
-   
-   while (current != NULL) {
-      
-      const sockaddr_in* interfaceAddress = castToIP4(current->ifa_addr);
-      const sockaddr_in* broadcastAddress = castToIP4(current->ifa_dstaddr);
-      const sockaddr_in* subnetMask       = castToIP4(current->ifa_netmask);
-      
-      printf("Interface %s", current->ifa_name);
-      if (current->ifa_addr != NULL) {
-         printf(" %s", levelToString(current->ifa_addr->sa_family));
-      }
-      printf("\nStatus    = %s\n", (current->ifa_flags & IFF_UP) ? "Online" : "Down");
-      printIP("IP       ", interfaceAddress);
-      printIP("Broadcast", broadcastAddress);
-      printIP("Subnet   ", subnetMask);
-      
-      // The MAC address and the interfaceAddress come in as
-      // different interfaces with the same name.
-      
-      if ((current->ifa_addr != NULL) && (current->ifa_addr->sa_family == AF_LINK)) {
-#           ifdef __linux__
-         // Linux
-         struct ifreq ifr;
-         
-         int fd = socket(AF_INET, SOCK_DGRAM, 0);
-         
-         ifr.ifr_addr.sa_family = AF_INET;
-         strcpy(ifr.ifr_name, current->ifa_name);
-         ioctl(fd, SIOCGIFHWADDR, &ifr);
-         close(fd);
-         
-         uint8_t* MAC = reinterpret_cast<uint8_t*>(ifr.ifr_hwaddr.sa_data);
-         
-#else
-         // Posix/FreeBSD/Mac OS
-         sockaddr_dl* sdl = (struct sockaddr_dl *)current->ifa_addr;
-         uint8_t* MAC = reinterpret_cast<uint8_t*>(LLADDR(sdl));
-#endif
-         printf("MAC       = %02x:%02x:%02x:%02x:%02x:%02x\n", MAC[0], MAC[1], MAC[2], MAC[3], MAC[4], MAC[5]);
-         
-         if (MAC[0]>0 or MAC[1]>0 or MAC[2]>0 or MAC[3]>0 or MAC[4]>0 or MAC[5]>0)
-         {
-            MAC_address_ss << hex << (int)MAC[0] << (int)MAC[1] << (int)MAC[2] << (int)MAC[3] << (int)MAC[4] << (int)MAC[5];
-            MAC_address = MAC_address_ss.str();
-            cout << MAC_address;
-         }
-      }
-      
-      printf("\n");
-      
-      current = current->ifa_next;
-   }
-   
-   freeifaddrs(ifap);
-   ifap = NULL;
-   
-   
-   return MAC_address;
-   
+   return 0;
 }
 
-
-
+*/
 
 
 #include <stdio.h>
@@ -1130,19 +993,6 @@ string get_cpu_freq_max(void)
 }
 
 
-/*
-uint64_t get_cpu_freq_min(void)
-{
-   uint64_t freq = 0;
-   size_t size = sizeof(freq);
-   
-   if (sysctlbyname("hw.cpufrequency_min", &freq, &size, NULL, 0) < 0)
-   {
-      perror("sysctl");
-   }
-   return freq;
-}
- */
 
 
 
