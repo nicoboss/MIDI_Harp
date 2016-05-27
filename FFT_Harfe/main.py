f3c2a8855e3b0cfd6aa8361e237460603378a813
nicobosshard <nico@bosshome.ch>
2016-05-27 21:40:36 +0200

    Mikrofon Implementierung

--- FFT_Harfe/main.py
+++ FFT_Harfe/main.py
@@ -3,27 +3,99 @@ FFT_Harfe von Nico Bosshard (c)2016
 '''
 
 from __future__ import print_function
-import os, sys, threading, math, time
+import os, sys, threading, math, time, threading, pyaudio, struct
+import numpy as np
 from random import *
 
 Code = []
 Data = []
+mic_chunks=[]
 
-mic=[44,63,88,24,65,87,98,9,12,23,34,45,56,67,78,89] #random numbers
+sampleRate=48100
+bufferSize = 2048
 
 comp=["==","!=","<",">"] #comparison operations
 oper=[" and ", " or "] #boolean operations later maybe useing binary bitwise operations
 moper=["+","*","**"] #arithmetic operations
 
-for a in range(1000):
-    cmd="if "
-    for i in range(0,2):
-        pos=mic.__len__()-i
-        cmd+="mic[pos-"+str(i)+"]"+comp[randint(0,3)]+str(randint(0,255))+oper[randint(0,1)]
-    cmd+="mic[pos-"+str(i)+"]"+comp[randint(0,3)]+str(randint(0,255)) + ": Data.append(1)\nelse: Data.append(0)"
-    Code.append(cmd)
-    #print(cmd)
-    exec(cmd)
-    print(Data[Data.__len__()-1],end='') #too slow
 
-#print(Data)
+def mic_stream():
+    global chunks, stream, bufferSize
+    #while True:
+    mic_chunks.append(stream.read(bufferSize))
+
+
+if __name__ == '__main__':
+
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    #for each audio device, determine if is an input or an output and add it to the appropriate list and dictionary
+    for i in range (0,numdevices):
+        if p.get_device_info_by_host_api_device_index(0,i).get('maxInputChannels')>0:
+            print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name'))
+        if p.get_device_info_by_host_api_device_index(0,i).get('maxOutputChannels')>0:
+            print("Output Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0,i).get('name'))
+    devinfo = p.get_device_info_by_index(1)
+    print("Selected device is ",devinfo.get('name')+"\n")
+
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1, rate=44100, input=True, output=True)
+
+    #threading.Thread(target=mic_stream).start()
+    mic_stream()
+    mic_data = mic_chunks.pop(0)
+    mic = np.array(struct.unpack("%dB"%(bufferSize*4),mic_data))
+    #s="["
+    #for q in mic:
+    #    s+=str(q) + ","
+    #s=s[:-1] + "]"
+    #print(s)
+
+    #if (1<2<3):print("Hi"
+
+    print("Generating source code...")
+    #f = open("Code.txt","w")
+    for i in range(bufferSize*4):
+        #mic.append(randint(0,255))
+        Subcode = []
+        Subcode_count=0
+        while Subcode_count<4:
+            #z1=randint(0,255)
+            #z2=randint(z1,255)
+            #cmd="if "+str(0)+"<mic[i]<"+str(0)+":Data.append(1)\nelse:Data.append(0)"
+            cmd="if mic[i]"+comp[randint(2,3)]+str(randint(0,255))+":a+=1"
+            a=0
+            exec(cmd)
+            if(a==1):
+                #f.write(cmd+"\n")
+                Subcode.append(cmd)
+                Subcode_count+=1
+        Code.append(Subcode)
+        #print(Subcode)
+        #print(Data[Data.__len__()-1],end='') #too slow
+    #print(Code)
+    #f.close()
+
+    print("done\n")
+
+
+    a=0
+    b=0
+    for i in range(bufferSize*4):
+        for e in Code[i]:
+            b+=1
+            exec(e)
+    print(str(a)+"/"+str(b))
+    print("done\n")
+    
+    a=0
+    b=0
+    for i in range(bufferSize*4):
+        for Subcode in Code:
+            for e in Subcode:
+                b+=1
+                exec(e)
+        print(str(a)+"/"+str(b))
+
+    #print(Data)
