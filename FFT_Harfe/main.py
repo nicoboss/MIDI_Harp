e5ab863d601461d50c7eb4ad71bedc25a15f3d2e
nicobosshard <nico@bosshome.ch>
2016-05-28 23:52:17 +0200

    Multiprocessing Implementierung

--- FFT_Harfe/main.py
+++ FFT_Harfe/main.py
@@ -3,7 +3,7 @@ FFT_Harfe von Nico Bosshard (c)2016
 '''
 
 from __future__ import print_function
-import os, sys, threading, math, time, threading, pyaudio, struct
+import os, sys, threading, math, time, threading, multiprocessing, pyaudio, struct
 import numpy as np
 from random import *
 
@@ -21,8 +21,17 @@ moper=["+","*","**"] #arithmetic operations
 
 def mic_stream():
     global chunks, stream, bufferSize
-    #while True:
-    mic_chunks.append(stream.read(bufferSize))
+    while True:
+        mic_chunks.append(stream.read(bufferSize))
+
+
+def mic_listen(bufferSize, Code = [], mic = []):
+    a=0
+    for i in range(0,bufferSize*4):
+        for e in Code[i]:
+            exec(e)
+    #print(mic)
+    return a
 
 
 if __name__ == '__main__':
@@ -40,62 +49,67 @@ if __name__ == '__main__':
     print("Selected device is ",devinfo.get('name')+"\n")
 
     stream = p.open(format=pyaudio.paFloat32,
-                    channels=1, rate=44100, input=True, output=True)
+                    channels=1, rate=48100, input=True, output=True)
 
-    #threading.Thread(target=mic_stream).start()
-    mic_stream()
-    mic_data = mic_chunks.pop(0)
-    mic = np.array(struct.unpack("%dB"%(bufferSize*4),mic_data))
-    #s="["
-    #for q in mic:
-    #    s+=str(q) + ","
-    #s=s[:-1] + "]"
-    #print(s)
+    threading.Thread(target=mic_stream).start()
+    time.sleep(1)
+    while True:
+        mic = mic_chunks.pop(0)
+        stream.write(mic)
+        #s="["
+        #for q in mic:
+        #    s+=str(q) + ","
+        #s=s[:-1] + "]"
+        #print(s)
 
-    #if (1<2<3):print("Hi"
+        #if (1<2<3):print("Hi"
 
-    print("Generating source code...")
-    #f = open("Code.txt","w")
-    for i in range(bufferSize*4):
-        #mic.append(randint(0,255))
-        Subcode = []
-        Subcode_count=0
-        while Subcode_count<4:
-            #z1=randint(0,255)
-            #z2=randint(z1,255)
-            #cmd="if "+str(0)+"<mic[i]<"+str(0)+":Data.append(1)\nelse:Data.append(0)"
-            cmd="if mic[i]"+comp[randint(2,3)]+str(randint(0,255))+":a+=1"
-            a=0
-            exec(cmd)
-            if(a==1):
-                #f.write(cmd+"\n")
-                Subcode.append(cmd)
-                Subcode_count+=1
-        Code.append(Subcode)
-        #print(Subcode)
-        #print(Data[Data.__len__()-1],end='') #too slow
-    #print(Code)
-    #f.close()
+        print("Generating source code...")
+        f = open("Code.txt","w")
+        for i in range(0,bufferSize*4,1):
+            #mic.append(randint(0,255))
+            Subcode = []
+            Subcode_count=0
+            while Subcode_count<30:
+                #z1=randint(0,255)
+                #z2=randint(z1,255)
+                #cmd="if "+str(0)+"<mic[i]<"+str(0)+":Data.append(1)\nelse:Data.append(0)"
+                cmd="if mic[i]"+comp[randint(2,3)]+str(randint(0,255))+":a+=1"
+                a=0
+                exec(cmd)
+                if(a==1):
+                    f.write(cmd+"\n")
+                    Subcode.append(cmd)
+                    Subcode_count+=1
+            Code.append(Subcode)
+            #print(Subcode)
+            #print(Data[Data.__len__()-1],end='') #too slow
+        #print(Code)
+        f.close()
+        break
 
     print("done\n")
+    #service = multiprocessing.Process(name='my_service', target=mic_listen, args=(mic_chunks.pop(0)))
+    #service.start()
+    multiprocessing.pool = multiprocessing.Pool(processes=1) # start 1 worker process
+    while True:
+        #RAM leack!
+        mic_chunks.pop(0)
+        result = multiprocessing.pool.apply_async(mic_listen, [bufferSize, Code, mic_chunks.pop(0),])
+        #stream.write(mic_chunks[-1])
+        #print(result.get())
+        #print result.get(timeout=1)
+        print(mic_chunks.__len__())
+        while mic_chunks.__len__()<5:
+            pass
 
-
-    a=0
-    b=0
-    for i in range(bufferSize*4):
-        for e in Code[i]:
-            b+=1
-            exec(e)
-    print(str(a)+"/"+str(b))
-    print("done\n")
-    
-    a=0
-    b=0
-    for i in range(bufferSize*4):
-        for Subcode in Code:
-            for e in Subcode:
-                b+=1
-                exec(e)
-        print(str(a)+"/"+str(b))
+##    a=0
+##    b=0
+##    for i in range(0,bufferSize/8):
+##        for Subcode in Code:
+##            for e in Subcode:
+##                b+=1
+##                exec(e)
+##        print(str(a)+"/"+str(b))
 
     #print(Data)
