c131e09897ba855e48c9a78522713e0ec3cd7c57
nicobosshard <nico@bosshome.ch>
2016-06-04 22:39:51 +0200

    Bugfixing sowie Implementierung der result_get-Funktion

--- FFT_Harfe/main.py
+++ FFT_Harfe/main.py
@@ -9,14 +9,17 @@ from random import *
 
 Code = []
 Data = []
+mic_chunks=[]
+result = []
+
 ChunkNr_shared = multiprocessing.Value('i', 0)
 Data_shared = multiprocessing.Array('b', 10000)
-mic_chunks=[]
 
 sampleRate=48100
 bufferSize = 2048
 
 AnzChunks=0
+AnzChunksDone=0
 
 comp=["==","!=","<",">"] #comparison operations
 oper=[" and ", " or "] #boolean operations later maybe useing binary bitwise operations
@@ -30,18 +33,27 @@ def mic_stream():
         AnzChunks+=1
 
 
-def mic_listen(ChunkNr, Data, bufferSize, Code = [], mic_data = []):
-    print("Hi")
+def mic_listen(bufferSize, Code = [], mic_data = []):
     mic = np.array(struct.unpack("%dB"%(bufferSize*4),mic_data))
-    print(mic)
     a=0
-    for i in range(0,bufferSize):
+    for i in range(0,bufferSize/4):
         for e in Code[i]:
             exec(e)
     #Data[ChunkNr.value]=a
     #ChunkNr.value=7
-    print(a)
+    #print(a, end=',')
     return a
+
+def result_get():
+    global AnzChunksDone
+    while True:
+        try:
+            result_temp = result[0].get()
+            result.pop(0)
+            AnzChunksDone+=1
+            print("Result: " + str(result_temp))
+        except:
+            pass
 
 
 if __name__ == '__main__':
@@ -61,9 +73,11 @@ if __name__ == '__main__':
     stream = p.open(format=pyaudio.paFloat32,
                     channels=1, rate=48100, input=True, output=True)
 
-    threading.Thread(target=mic_stream).start()
+    mic_thread = threading.Thread(target=mic_stream)
+    mic_thread.start()
+    f = open("Code.txt","w")
     time.sleep(0.2)
-    while True:
+    for i in range(15):
         mic_data=mic_chunks.pop(0)
         mic = np.array(struct.unpack("%dB"%(bufferSize*4),mic_data))
         stream.write(mic_data)
@@ -73,31 +87,38 @@ if __name__ == '__main__':
         #s=s[:-1] + "]"
         #print(s)
 
-        #if (1<2<3):print("Hi"
+        #if (1<2<3):print("Hi")
 
-        print("Generating source code...")
-        f = open("Code.txt","w")
-        for i in range(0,bufferSize*4,4):
+        print("Generating source code...   " + str(i))
+        for i in range(0,bufferSize*4,16):
             #mic.append(randint(0,255))
             Subcode = []
             Subcode_count=0
-            while Subcode_count<4:
+            while Subcode_count<2:
                 #z1=randint(0,255)
                 #z2=randint(z1,255)
                 #cmd="if "+str(0)+"<mic[i]<"+str(0)+":Data.append(1)\nelse:Data.append(0)"
-                cmd="if mic[i]"+comp[randint(2,3)]+str(randint(0,255))+":a+=1"
+                cmd_mid=comp[randint(2,3)]+str(randint(0,255))
+                cmd="if mic[i]" + cmd_mid + ":a+=1"
+                cmd_check="if testchunk[randint(0," + str(bufferSize) + ")]" + cmd_mid + ":t+=1"
                 a=0
                 exec(cmd)
                 if(a==1):
-                    f.write(cmd+"\n")
-                    Subcode.append(cmd)
-                    Subcode_count+=1
+                    testchunk=mic_chunks[-1]
+                    t=0
+                    for test in range(30):
+                        exec(cmd_check)
+                    if t<10:
+                         f.write(cmd+"\n")
+                         Subcode.append(cmd)
+                         Subcode_count+=1
             Code.append(Subcode)
             #print(Subcode)
             #print(Data[Data.__len__()-1],end='') #too slow
-        #print(Code)
-        f.close()
-        break
+            #print(Code)
+            if mic_chunks.__len__()<3: break
+    f.close()
+        
 
     print("done\n")
     #service = multiprocessing.Process(name='my_service', target=mic_listen, args=(mic_chunks.pop(0)))
@@ -113,23 +134,29 @@ if __name__ == '__main__':
 ##                exec(e)
 ##        print(a)
 
-
     
     AnzChunks=0
     mic_chunks = []
+
+    threading.Thread(target=result_get).start()
 
     #Shared memory for pool of workers how to get this work?
     multiprocessing.pool = multiprocessing.Pool(processes=8) # start 1 worker process
     while True:
         while mic_chunks.__len__()<5:
             pass
-        result = multiprocessing.pool.apply_async(mic_listen, [ChunkNr_shared, Data_shared, bufferSize, Code, mic_chunks.pop(0),])
+        result.append(multiprocessing.pool.apply_async(mic_listen, [bufferSize, Code, mic_chunks.pop(0),]))
         #stream.write(mic_chunks[-1])
         #print(Data_shared[:20])
-        print(ChunkNr_shared.value)
         #print(result.get(timeout=1))
+        #######print(str(AnzChunksDone) + "/" + str(AnzChunks))
         #print("Az:" + str(AnzChunks) + "Ln:" + str(mic_chunks.__len__()),end='')
 
+
+    
+    while True:
+        pass
+    #time.sleep()
 ##    a=0
 ##    b=0
 ##    for i in range(0,bufferSize/8):,
