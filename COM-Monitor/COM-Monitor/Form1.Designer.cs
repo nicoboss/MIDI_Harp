75ed3174c5e71cb4a667cdd8ea0d92ca6770a6dc
nicobosshard <nico@bosshome.ch>
2016-07-22 00:28:29 +0200

    Start der Programmierung des COM-Monitor-Tools

---
+++ COM-Monitor/COM-Monitor/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace COM_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Connect_GroupBox = new System.Windows.Forms.GroupBox();
+            this.ComboBox_Comport = new System.Windows.Forms.ComboBox();
+            this.Button_Disconnect = new System.Windows.Forms.Button();
+            this.Button_Connect = new System.Windows.Forms.Button();
+            this.Com_Search_Timer = new System.Windows.Forms.Timer(this.components);
+            this.Messintervall_GroupBox = new System.Windows.Forms.GroupBox();
+            this.Messintervall_TextBox = new System.Windows.Forms.TextBox();
+            this.MessungenProS_TexBox = new System.Windows.Forms.TextBox();
+            this.Messintervall_Label = new System.Windows.Forms.Label();
+            this.GroupBox3 = new System.Windows.Forms.GroupBox();
+            this.Anz_Messungen_TextBox = new System.Windows.Forms.TextBox();
+            this.Anz_Verbindungsfehler_TextBox = new System.Windows.Forms.TextBox();
+            this.Serial_BackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.SerialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Connect_GroupBox.SuspendLayout();
+            this.Messintervall_GroupBox.SuspendLayout();
+            this.GroupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Connect_GroupBox
+            // 
+            this.Connect_GroupBox.Controls.Add(this.ComboBox_Comport);
+            this.Connect_GroupBox.Controls.Add(this.Button_Disconnect);
+            this.Connect_GroupBox.Controls.Add(this.Button_Connect);
+            this.Connect_GroupBox.Location = new System.Drawing.Point(13, 14);
+            this.Connect_GroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Connect_GroupBox.Name = "Connect_GroupBox";
+            this.Connect_GroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Connect_GroupBox.Size = new System.Drawing.Size(448, 88);
+            this.Connect_GroupBox.TabIndex = 189;
+            this.Connect_GroupBox.TabStop = false;
+            this.Connect_GroupBox.Text = "Comport - Verbindungseinstellungen für Mikrokontroller";
+            // 
+            // ComboBox_Comport
+            // 
+            this.ComboBox_Comport.FormattingEnabled = true;
+            this.ComboBox_Comport.Location = new System.Drawing.Point(315, 32);
+            this.ComboBox_Comport.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ComboBox_Comport.Name = "ComboBox_Comport";
+            this.ComboBox_Comport.Size = new System.Drawing.Size(109, 28);
+            this.ComboBox_Comport.Sorted = true;
+            this.ComboBox_Comport.TabIndex = 6;
+            this.ComboBox_Comport.SelectedIndexChanged += new System.EventHandler(this.ComboBox_Comport_SelectedIndexChanged);
+            // 
+            // Button_Disconnect
+            // 
+            this.Button_Disconnect.Location = new System.Drawing.Point(130, 29);
+            this.Button_Disconnect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Button_Disconnect.Name = "Button_Disconnect";
+            this.Button_Disconnect.Size = new System.Drawing.Size(112, 35);
+            this.Button_Disconnect.TabIndex = 5;
+            this.Button_Disconnect.Text = "Trennen";
+            this.Button_Disconnect.UseVisualStyleBackColor = true;
+            this.Button_Disconnect.Click += new System.EventHandler(this.Button_Disconnect_Click);
+            // 
+            // Button_Connect
+            // 
+            this.Button_Connect.Location = new System.Drawing.Point(9, 29);
+            this.Button_Connect.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Button_Connect.Name = "Button_Connect";
+            this.Button_Connect.Size = new System.Drawing.Size(112, 35);
+            this.Button_Connect.TabIndex = 4;
+            this.Button_Connect.Text = "Verbinden";
+            this.Button_Connect.UseVisualStyleBackColor = true;
+            this.Button_Connect.Click += new System.EventHandler(this.Button_Connect_Click);
+            // 
+            // Com_Search_Timer
+            // 
+            this.Com_Search_Timer.Enabled = true;
+            this.Com_Search_Timer.Interval = 2000;
+            this.Com_Search_Timer.Tick += new System.EventHandler(this.Com_Search_Timer_Tick);
+            // 
+            // Messintervall_GroupBox
+            // 
+            this.Messintervall_GroupBox.Controls.Add(this.Messintervall_TextBox);
+            this.Messintervall_GroupBox.Controls.Add(this.MessungenProS_TexBox);
+            this.Messintervall_GroupBox.Controls.Add(this.Messintervall_Label);
+            this.Messintervall_GroupBox.Location = new System.Drawing.Point(482, 24);
+            this.Messintervall_GroupBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Messintervall_GroupBox.Name = "Messintervall_GroupBox";
+            this.Messintervall_GroupBox.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Messintervall_GroupBox.Size = new System.Drawing.Size(282, 78);
+            this.Messintervall_GroupBox.TabIndex = 191;
+            this.Messintervall_GroupBox.TabStop = false;
+            this.Messintervall_GroupBox.Text = "Messintervall vom Mikrokontroller";
+            // 
+            // Messintervall_TextBox
+            // 
+            this.Messintervall_TextBox.Enabled = false;
+            this.Messintervall_TextBox.Location = new System.Drawing.Point(183, 34);
+            this.Messintervall_TextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Messintervall_TextBox.Name = "Messintervall_TextBox";
+            this.Messintervall_TextBox.ReadOnly = true;
+            this.Messintervall_TextBox.Size = new System.Drawing.Size(85, 26);
+            this.Messintervall_TextBox.TabIndex = 181;
+            this.Messintervall_TextBox.Text = "0 ms";
+            // 
+            // MessungenProS_TexBox
+            // 
+            this.MessungenProS_TexBox.Enabled = false;
+            this.MessungenProS_TexBox.Location = new System.Drawing.Point(86, 34);
+            this.MessungenProS_TexBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MessungenProS_TexBox.Name = "MessungenProS_TexBox";
+            this.MessungenProS_TexBox.ReadOnly = true;
+            this.MessungenProS_TexBox.Size = new System.Drawing.Size(85, 26);
+            this.MessungenProS_TexBox.TabIndex = 180;
+            this.MessungenProS_TexBox.Text = "0 M/s";
+            // 
+            // Messintervall_Label
+            // 
+            this.Messintervall_Label.AutoSize = true;
+            this.Messintervall_Label.Location = new System.Drawing.Point(14, 38);
+            this.Messintervall_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Messintervall_Label.Name = "Messintervall_Label";
+            this.Messintervall_Label.Size = new System.Drawing.Size(58, 20);
+            this.Messintervall_Label.TabIndex = 179;
+            this.Messintervall_Label.Text = "Mes/S:";
+            // 
+            // GroupBox3
+            // 
+            this.GroupBox3.Controls.Add(this.Anz_Messungen_TextBox);
+            this.GroupBox3.Controls.Add(this.Anz_Verbindungsfehler_TextBox);
+            this.GroupBox3.Location = new System.Drawing.Point(782, 33);
+            this.GroupBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GroupBox3.Name = "GroupBox3";
+            this.GroupBox3.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.GroupBox3.Size = new System.Drawing.Size(298, 69);
+            this.GroupBox3.TabIndex = 192;
+            this.GroupBox3.TabStop = false;
+            this.GroupBox3.Text = "Anz.Messungen + Verbindungsfehler";
+            // 
+            // Anz_Messungen_TextBox
+            // 
+            this.Anz_Messungen_TextBox.Enabled = false;
+            this.Anz_Messungen_TextBox.Location = new System.Drawing.Point(18, 31);
+            this.Anz_Messungen_TextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Anz_Messungen_TextBox.Name = "Anz_Messungen_TextBox";
+            this.Anz_Messungen_TextBox.ReadOnly = true;
+            this.Anz_Messungen_TextBox.Size = new System.Drawing.Size(124, 26);
+            this.Anz_Messungen_TextBox.TabIndex = 0;
+            this.Anz_Messungen_TextBox.Text = "0";
+            this.Anz_Messungen_TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Anz_Verbindungsfehler_TextBox
+            // 
+            this.Anz_Verbindungsfehler_TextBox.Enabled = false;
+            this.Anz_Verbindungsfehler_TextBox.Location = new System.Drawing.Point(153, 31);
+            this.Anz_Verbindungsfehler_TextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Anz_Verbindungsfehler_TextBox.Name = "Anz_Verbindungsfehler_TextBox";
+            this.Anz_Verbindungsfehler_TextBox.ReadOnly = true;
+            this.Anz_Verbindungsfehler_TextBox.Size = new System.Drawing.Size(134, 26);
+            this.Anz_Verbindungsfehler_TextBox.TabIndex = 187;
+            this.Anz_Verbindungsfehler_TextBox.Text = "0";
+            this.Anz_Verbindungsfehler_TextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Serial_BackgroundWorker
+            // 
+            this.Serial_BackgroundWorker.WorkerSupportsCancellation = true;
+            this.Serial_BackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // SerialPort1
+            // 
+            this.SerialPort1.BaudRate = 230400;
+            this.SerialPort1.DtrEnable = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 110);
+            this.textBox1.MaxLength = 0;
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(1067, 576);
+            this.textBox1.TabIndex = 193;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 698);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.GroupBox3);
+            this.Controls.Add(this.Messintervall_GroupBox);
+            this.Controls.Add(this.Connect_GroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Connect_GroupBox.ResumeLayout(false);
+            this.Messintervall_GroupBox.ResumeLayout(false);
+            this.Messintervall_GroupBox.PerformLayout();
+            this.GroupBox3.ResumeLayout(false);
+            this.GroupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.GroupBox Connect_GroupBox;
+        internal System.Windows.Forms.ComboBox ComboBox_Comport;
+        internal System.Windows.Forms.Button Button_Disconnect;
+        internal System.Windows.Forms.Button Button_Connect;
+        internal System.Windows.Forms.Timer Com_Search_Timer;
+        internal System.Windows.Forms.GroupBox Messintervall_GroupBox;
+        internal System.Windows.Forms.TextBox Messintervall_TextBox;
+        internal System.Windows.Forms.TextBox MessungenProS_TexBox;
+        internal System.Windows.Forms.Label Messintervall_Label;
+        internal System.Windows.Forms.GroupBox GroupBox3;
+        internal System.Windows.Forms.TextBox Anz_Messungen_TextBox;
+        internal System.Windows.Forms.TextBox Anz_Verbindungsfehler_TextBox;
+        internal System.ComponentModel.BackgroundWorker Serial_BackgroundWorker;
+        internal System.IO.Ports.SerialPort SerialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ COM-Monitor/COM-Monitor/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace COM_Monitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        Populates the COM List. Is executed once per second. 
+        Handler of the Com_Search_Timer.Tick event.
+        */
+        private void Com_Search_Timer_Tick(object sender, EventArgs e)
+        {
+            var ports = SerialPort.GetPortNames();
+            ComboBox_Comport.Items.Clear();
+
+            //Comports suchen
+            foreach (var port in ports)
+            {
+                ComboBox_Comport.Items.Add(port);
+            }
+
+            ComboBox_Comport.Sorted = true;
+        }
+
+        private void Button_Connect_Click(object sender, EventArgs e)
+        {
+            //verbinden
+            try
+            {
+                //Buttons freigeben-/sperren
+                Button_Connect.Enabled = false;
+                Button_Disconnect.Enabled = true;
+                ComboBox_Comport.Enabled = false;
+
+                //Comport öffnen
+                SerialPort1.PortName = ComboBox_Comport.Text;
+                SerialPort1.BaudRate = 230400; //Baudrate
+                SerialPort1.DataBits = 8;
+                SerialPort1.Encoding = System.Text.Encoding.Default;
+                SerialPort1.Open();
+
+
+                Com_Search_Timer.Enabled = false;
+                Serial_BackgroundWorker.RunWorkerAsync();
+            }
+            catch (Exception ex)
+            {
+                //Fehlermeldung
+                MessageBox.Show("Unter dem gewählten COM Port wurde kein Mikrokontroller erkannt. " + Environment.NewLine +
+                                "Stellen Sie sicher, das Sie den Richtigen Port gewählt haben und der" + Environment.NewLine +
+                                "Mikrokontroller korrekt mit dem Computer verbunden, und" + Environment.NewLine +
+                                "nicht schon von einem anderen Programm besetzt ist." + Environment.NewLine +
+                                "Fehler: " + ex.ToString() + Environment.NewLine +
+                                "Support E-Mail Adresse: nico@bosshome.ch" + Environment.NewLine +
+                                "Fehlercode: 1 (Verbindungsfehler)",
+                                "Verbindungsfehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Button_Connect.Enabled = true;
+                Button_Disconnect.Enabled = false;
+                ComboBox_Comport.Enabled = true;
+            }
+        }
+
+        private void Button_Disconnect_Click(object sender, EventArgs e)
+        {
+            /*
+            Handler for the disconnect button and programm close event
+            Stop the serial connection thread. Clean up GUI. 
+            */
+            Serial_BackgroundWorker.CancelAsync();
+            Anz_Messungen_TextBox.Text = "0";
+            Anz_Verbindungsfehler_TextBox.Text = "0";
+            MessungenProS_TexBox.Text = "0 M/s";
+            Messintervall_TextBox.Text = "0 ms";
+            Com_Search_Timer.Enabled = true;
+            Button_Connect.Enabled = true;
+            Button_Disconnect.Enabled = false;
+            ComboBox_Comport.Enabled = true;
+            SerialPort1.Close();
+        }
+
+        private void ComboBox_Comport_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Comport auswählen
+            if (ComboBox_Comport.SelectedItem != "")
+            {
+                Button_Connect.Enabled = true;
+            }
+        }
+
+        private void SerialPort1_DataReceived(object sender, DoWorkEventArgs e)
+        {
+            var ShowResultsCount = 0;
+            double Messintervall_Zahl = 0;
+            double Anz_Messungen = 0;
+            double AnzMessungen_alt = 0;
+            var Anz_Verbindungsfehler = 0;
+            Anz_Messungen_TextBox.Text = "0";
+            Anz_Verbindungsfehler_TextBox.Text = "0";
+            Stopwatch stopWatch = new Stopwatch();
+
+            //while (SerialPort1.ReadByte() != 250) ;
+            int serial;
+
+            stopWatch.Start();
+            while (true)
+            {
+                while (true)
+                {
+                    //serial = SerialPort1.ReadByte();
+                    //MessageBox.Show(SerialPort1.ReadByte().ToString());
+                    //Invoke((MethodInvoker)delegate ()
+                    //{
+                    //    textBox1.AppendText(((char)serial).ToString());
+                    //});
+                    if (SerialPort1.ReadByte() == 13)
+                    {
+                        break;
+                    }
+                    //Console.WriteLine(SerialPort1.ReadByte().ToString());
+                }
+                ShowResultsCount++;
+                Anz_Messungen++;
+                if (ShowResultsCount == 500)
+                {
+                    stopWatch.Stop();
+                    //MessageBox.Show("");
+                    ShowResultsCount = 0;
+                    Invoke((MethodInvoker)delegate ()
+                    {
+                        try
+                        {
+                            //250ms
+                            Anz_Messungen_TextBox.Text = Anz_Messungen.ToString();
+                            //Anz_Messungen_TextBox.Text = (Anz_Messungen - AnzMessungen_alt).ToString();
+                            //Anz_Messungen_TextBox.Text = stopWatch.ElapsedMilliseconds.ToString();
+                            Messintervall_Zahl = (Anz_Messungen - AnzMessungen_alt) / (stopWatch.ElapsedMilliseconds / 1000.0); //Achtung: Komischerweise Divison durch 0 wenn AnzMessungen_alt > Anz_Messungen! Wieso habe ich keine Ahnung.
+                            MessungenProS_TexBox.Text = String.Format(Messintervall_Zahl.ToString(), "000") + " M/s";
+                            Messintervall_TextBox.Text = String.Format((1000 / Messintervall_Zahl).ToString(), "00.0") + " ms";
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString());
+                        }
+
+                        AnzMessungen_alt = Anz_Messungen;
+                        //textBox1.AppendText(Environment.NewLine);
+                        stopWatch.Restart();
+                    });
+                }
+                
+            }
+        }
+
+    }
+}
+
